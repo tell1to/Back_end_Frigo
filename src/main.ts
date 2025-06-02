b6339e841e7e2b7af5e0750a6676b713adc2e396
tell1to <hmd.murillo.edu.ec>
2025-06-01 23:17:28 -0500

    conexion-habilitada

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
     const PORT = process.env.PORT || 3000;
   await app.listen(PORT, '127.0.0.1');
   Logger.log(`Application running on: http://localhost:${PORT}`, 'Bootstrap');
